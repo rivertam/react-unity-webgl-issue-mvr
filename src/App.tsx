80943156f14a57588aa18dc5bfff0c7662c0072a
Ben Berman <ben.m.berman@gmail.com>
2018-12-28 10:58:56 -0500

    Add react-unity-webgl

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Unity, { UnityContent } from 'react-unity-webgl';
 
 class App extends Component {
   render() {
